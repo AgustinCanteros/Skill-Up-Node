e2c7d5c37fc274571646ad8d57feb08ef6113e1a
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-11-01 23:34:04 -0500

    corrigiendo errores de funcionamiento

--- routes/transactions/transactions.routes.js
+++ routes/transactions/transactions.routes.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { getTransactionsById } = require('../controllers/transactions/transactions.controller')
+const { getTransactionsById } = require('../../controllers/transactions/transactions.controller')
 
 const router = express.Router();
 
 
-router.get('/transactions/?query=', getTransactionsById);
+router.get('/transactions', getTransactionsById);
 
 module.exports = router
