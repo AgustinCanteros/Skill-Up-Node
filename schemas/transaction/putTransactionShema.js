203d6c3320a4085cab518f574a4f45770e2afc26
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-11-03 22:35:54 -0500

    adding schema and route validations

---
+++ schemas/transaction/putTransactionShema.js
@@ -0,0 +1,46 @@
+const { checkShecma } = require("express-validator");
+const { Transactions } = require("../../database/models");
+const { ErrorObject } = require("../../helpers");
+
+module.exports = {
+  putValidation: checkShecma({
+    id: {
+      in: ["params", "query"],
+      errorMessage: "ID is wrong",
+      isInt: true,
+      toInt: true,
+      custom: {
+        options: async ({ req }) => {
+          const validation = await Transactions.findOne({
+            where: { id: `${req.params.id}` },
+          });
+          if (!validation) {
+            throw new ErrorObject("id the transaction don't exist", 404);
+          }
+        },
+      },
+    },
+    userId: {
+      in: ["body"],
+      errorMessage: "User in the transaction don't exist",
+      isInt: true,
+      toInt: true,
+    },
+    categoryId: {
+      in: ["body"],
+      errorMessage: "Category in the transaction don't exist",
+      isInt: true,
+      toInt: true,
+    },
+    amount: {
+      in: ["body"],
+      errorMessage: "amount in the transaction don't exist",
+    },
+    date: {
+      in: ["body"],
+      errorMessage: "amount in the transaction don't exist",
+      isDate: true,
+      toDate: true,
+    },
+  }),
+};
