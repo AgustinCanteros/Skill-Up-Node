371e08625cd1c101a42036334adda10c47b1d27d
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-11-04 13:22:33 -0500

    changing file locations

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -1,7 +1,7 @@
 const createHttpError = require("http-errors");
-const { endpointResponse } = require("../../helpers/success");
-const { catchAsync } = require("../../helpers/catchAsync");
-const { Transactions } = require("../../database/models");
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
+const { Transactions } = require("../database/models");
 
 module.exports = {
   destroy: catchAsync(async (req, res, next) => {
