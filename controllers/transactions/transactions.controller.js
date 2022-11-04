8c05184d62e9fc81fddf9574798e82e5331ca923
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-11-03 22:37:04 -0500

    correcting the controller

--- controllers/transactions/transactions.controller.js
+++ controllers/transactions/transactions.controller.js
@@ -6,25 +6,14 @@ const { Transactions } = require("../../database/models");
 module.exports = {
   put: catchAsync(async (req, res, next) => {
     try {
-      const id = req.params.id;
-      const validation = await Transactions.findAll( {
+      const response = await Transactions.update(req.body, {
         where: { id: `${id}` },
       });
-      if (validation) {
-        const response = await Transactions.update(req.body, {
-          where: { id: `${id}` },
-        });
-        endpointResponse({
-          res,
-          message: "successfully",
-          body: response,
-        });
-      } else {
-        const httpError = createHttpError(
-          `[Error retrieving transaction] - [transaction - PUT]: id the transaction don't exist`
-        );
-        next(httpError);
-      }
+      endpointResponse({
+        res,
+        message: "successfully",
+        body: response,
+      });
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
