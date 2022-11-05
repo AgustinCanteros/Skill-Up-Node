b6eb487ff47252ba4fa331c7c8ad387b6770cd96
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-11-04 22:04:45 -0500

    changes name of controller

--- controllers/transactions.controllers.js
+++ controllers/transactions.controllers.js
@@ -1,44 +1,9 @@
-const createHttpError = require('http-errors');
-const { Transaction } = require('../database/models');
-const { endpointResponse } = require('../helpers/success');
-const { catchAsync } = require('../helpers/catchAsync');
+const createHttpError = require("http-errors");
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
+const { Transactions } = require("../database/models");
 
-// example of a controller. First call the service, then build the controller method
 module.exports = {
-  postCreateTransaction: catchAsync(async (req, res, next) => {
-    try {
-      const { description, amount, date, userId, categoryId, user, category } =
-        req.body;
-
-      await Transaction.create({
-        description,
-        amount,
-        date,
-        userId,
-        categoryId,
-      });
-
-      const body = {
-        description,
-        amount,
-        date,
-        user,
-        category,
-      };
-
-      endpointResponse({
-        res,
-        message: 'Transaction created successfully.',
-        body,
-      });
-    } catch (error) {
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error retrieving Transaction] - [Transaction - POST]: ${error.message}`
-      );
-      next(httpError);
-    }
-  }),
   put: catchAsync(async (req, res, next) => {
     try {
       const id = req.params.id;
