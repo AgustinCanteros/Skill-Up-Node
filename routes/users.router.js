92499004ed4b23d15aac2de8a0cf590b75b6578f
arielvillafuerte <arielcesarantoniovillafuerte@gmail.com>
2022-11-06 16:12:09 -0300

    comments were removed

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { createUsers, deleteUser } = require("../controllers/user.controller");
 const createUserSchema = require("../schemas/user/createUserSchema");
-const deleteUserSchema = require("../schemas/user/deleteUserSchema")
+const deleteUserSchema = require("../schemas/user/deleteUserSchema");
 const {
   validateRequestSchema,
 } = require("../middlewares/validation/validate-schema.middleware");
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post("/", validateRequestSchema(createUserSchema), createUsers);
 router.get("/", getAllUsers);
-router.delete("/:id", validateRequestSchema(deleteUserSchema), deleteUser)
+router.delete("/:id", validateRequestSchema(deleteUserSchema), deleteUser);
 
 module.exports = router;
