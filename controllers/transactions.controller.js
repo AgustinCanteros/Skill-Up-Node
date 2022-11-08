85fd3b187ad1df273e94640d54809a4b4335e51b
arielvillafuerte <arielcesarantoniovillafuerte@gmail.com>
2022-11-08 00:20:49 -0300

    errors were eliminated

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -2,15 +2,12 @@ const createHttpError = require("http-errors");
 const { Transactions } = require("../database/models");
 const { endpointResponse } = require("../helpers/success");
 const { catchAsync } = require("../helpers/catchAsync");
-const { ErrorObject } = require("../helpers/error");
 
 module.exports = {
   postCreateTransaction: catchAsync(async (req, res, next) => {
     try {
       const response = await Transactions.create(req.body);
-      if (!response) {
-        throw new ErrorObject("The transaction could not be found", 404);
-      }
+
       endpointResponse({
         res,
         message: "Transaction created successfully.",
@@ -51,7 +48,7 @@ module.exports = {
       endpointResponse({
         res,
         message: "successfully",
-        body: req.body,
+        body: response,
       });
     } catch (error) {
       const httpError = createHttpError(
@@ -76,15 +73,15 @@ module.exports = {
         });
       } else {
         response.length
-        ? endpointResponse({
-            res,
-            message: "Transactions obtained successfully",
-            body: response,
-          })
-        : endpointResponse({
-            res,
-            message: "No Transactions on DB",
-          });
+          ? endpointResponse({
+              res,
+              message: "Transactions obtained successfully",
+              body: response,
+            })
+          : endpointResponse({
+              res,
+              message: "No Transactions on DB",
+            });
       }
     } catch (error) {
       const httpError = createError(
@@ -97,9 +94,12 @@ module.exports = {
   deleteTransaction: catchAsync(async (req, res, next) => {
     try {
       const id = req.params.id;
-      const response = await Transactions.update({softDeletes:new Date()}, {
-        where: { id: `${id}` },
-      });
+      const response = await Transactions.update(
+        { softDeletes: new Date() },
+        {
+          where: { id: `${id}` },
+        }
+      );
       endpointResponse({
         res,
         message: "successfully, transaction deleted",
