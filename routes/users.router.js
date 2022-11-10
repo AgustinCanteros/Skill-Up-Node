3c2d1fb53a31f153116f49a90673569d36c30f77
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-11-09 20:30:32 -0500

    Document Users endpoints

--- routes/users.router.js
+++ routes/users.router.js
@@ -15,10 +15,121 @@ const {
 const getByIdSchema = require("../schemas/user/getByIdSchema");
 const router = express.Router();
 
-router.post("/", validateRequestSchema(createUserSchema), createUsers);
+/**
+ * @swagger
+ * /users:
+ *   tags:
+ *   -User
+ *   get:
+ *     summary: Find all users
+ *     description: Returns all users
+ *     responses:
+ *       200:
+ *         description: successful operation
+ *         schema:
+ *          $ref: #/definitions/users
+ *     security:
+ *     - api_key: []
+ *   post:
+ *     summary: Create user
+ *     description: This can only be done by the logged in user.
+ *     parameters:
+ *     - name: body
+ *        in: body
+ *        required: true
+ *        description: Created user object
+ *        required: true
+ *        schema:
+ *         $ref: #/definitions/users
+ *     responses:
+ *       200:
+ *         description: successful operation
+ *         schema:
+ *          $ref: #/definitions/users
+ *       400:
+ *         description: Bad Request
+ */
 router.get("/", getAllUsers);
+router.post("/", validateRequestSchema(createUserSchema), createUsers);
+
+/**
+ * @swagger
+ * /users/{userId}:
+ *   tags:
+ *   -User
+ *   get:
+ *     summary: Find user by Id
+ *     parameters:
+ *     - name: userId
+ *        in: path
+ *        required: true
+ *        description: ID of user to return.
+ *        schema:
+ *           type: integer
+ *        example: 1
+ *     responses:
+ *       200:
+ *         description: successful operation
+ *         schema:
+ *           $ref: #/definitions/users
+ *       400:
+ *         description: The specified user ID is invalid (not a number).
+ *       404:
+ *         description: A user with the specified ID was not found.
+ *       default:
+ *         description: Unexpected error
+ *     security:
+ *     - api_key: []
+ *   put:
+ *     summary: Edit user by Id
+ *     parameters:
+ *     - name: userId
+ *        in: path
+ *        required: true
+ *        description: ID of user to return.
+ *        schema:
+ *           type: integer
+ *        example: 1
+ *     responses:
+ *       200:
+ *         description: successful operation
+ *         schema:
+ *           $ref: #/definitions/users
+ *       400:
+ *         description: The specified user ID is invalid (not a number).
+ *       404:
+ *         description: A user with the specified ID was not found.
+ *       default:
+ *         description: Unexpected error
+ *     security:
+ *     - api_key: []
+ *   delete:
+ *     summary: Delete user by Id
+ *     parameters:
+ *     - name: userId
+ *        in: path
+ *        required: true
+ *        description: ID of user to return.
+ *        schema:
+ *           type: integer
+ *        example: 1
+ *     responses:
+ *       200:
+ *         description: successful operation
+ *         schema:
+ *           $ref: #/definitions/users
+ *       400:
+ *         description: The specified user ID is invalid (not a number).
+ *       404:
+ *         description: A user with the specified ID was not found.
+ *       default:
+ *         description: Unexpected error
+ *     security:
+ *     - api_key: []
+ */
 router.get("/:id", validateRequestSchema(getByIdSchema), getUserById);
-router.delete("/:id", validateRequestSchema(deleteUserSchema), deleteUser);
 router.put("/:id", validateRequestSchema(editUserSchema), editUser);
+router.delete("/:id", validateRequestSchema(deleteUserSchema), deleteUser);
+
 
 module.exports = router;
