7ad6474c856af8ab2c615093fce12b9a2ccc5887
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-11-11 09:08:59 -0500

    correcting users docs

--- routes/users.router.js
+++ routes/users.router.js
@@ -18,114 +18,144 @@ const router = express.Router();
 /**
  * @swagger
  * /users:
- *   tags:
- *   -User
  *   get:
  *     summary: Find all users
+ *     tags: [users]
  *     description: Returns all users
  *     responses:
- *       200:
+ *       '200':
  *         description: successful operation
- *         schema:
- *          $ref: #/definitions/users
- *     security:
- *     - api_key: []
+*          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/category'
  *   post:
  *     summary: Create user
+ *     tags: [users]
  *     description: This can only be done by the logged in user.
- *     parameters:
- *     - name: body
- *        in: body
- *        required: true
- *        description: Created user object
- *        required: true
- *        schema:
- *         $ref: #/definitions/users
+ *     request body:
+ *          description: Get all users
+ *          content:
+ *             application/json:
+ *               schema: $ref: #/components/schemas/users
+ *             application/xml:
+ *                schema:
+ *                  $ref: '#/components/schemas/users'
+ *             application/x-www-form-urlencoded:
+ *                schema:
+ *                  $ref: '#/components/schemas/users'
+ *            required: true
  *     responses:
- *       200:
+ *       '200':
  *         description: successful operation
- *         schema:
- *          $ref: #/definitions/users
- *       400:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/users'
+ *       '400':
  *         description: Bad Request
+ *       '500':
+ *         description: error of server
  */
 router.get("/", getAllUsers);
 router.post("/", validateRequestSchema(createUserSchema), createUsers);
 
 /**
  * @swagger
- * /users/{userId}:
- *   tags:
- *   -User
+ * /users/{id}:
  *   get:
  *     summary: Find user by Id
+ *     tags: [users]
  *     parameters:
- *     - name: userId
- *        in: path
- *        required: true
- *        description: ID of user to return.
- *        schema:
- *           type: integer
- *        example: 1
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          description: ID of user to return.
+ *          schema:
+ *             type: integer
+ *             format: int64
  *     responses:
- *       200:
+ *       '200':
  *         description: successful operation
- *         schema:
- *           $ref: #/definitions/users
- *       400:
+ *         content:
+ *            application/json:
+ *               schema:
+ *                 $ref: #/components/schemas/users
+ *       '400':
  *         description: The specified user ID is invalid (not a number).
- *       404:
+ *       '404':
  *         description: A user with the specified ID was not found.
- *       default:
+ *       '500':
+ *         description: error of server.
+ *       'default':
  *         description: Unexpected error
- *     security:
- *     - api_key: []
  *   put:
  *     summary: Edit user by Id
+ *     tags: [users]
  *     parameters:
- *     - name: userId
- *        in: path
- *        required: true
- *        description: ID of user to return.
- *        schema:
- *           type: integer
- *        example: 1
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          description: ID of user to return.
+ *          schema:
+ *             type: integer
+ *             format: int64
+ *     requestBody:
+ *            description: Update a user by id 
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/users'
+ *              application/xml:
+ *                schema:
+ *                  $ref: '#/components/schemas/users'
+ *              application/x-www-form-urlencoded:
+ *                schema:
+ *                  $ref: '#/components/schemas/users'
+ *            required: true
  *     responses:
- *       200:
+ *       '200':
  *         description: successful operation
- *         schema:
- *           $ref: #/definitions/users
- *       400:
+ *         content:
+ *            application/json:
+ *               schema:
+ *                 $ref: #/components/schemas/users
+ *       '400':
  *         description: The specified user ID is invalid (not a number).
- *       404:
+ *       '404':
  *         description: A user with the specified ID was not found.
- *       default:
+ *       '500':
+ *         description: error of server.
+ *       'default':
  *         description: Unexpected error
- *     security:
- *     - api_key: []
  *   delete:
  *     summary: Delete user by Id
+ *     tags: [users]
  *     parameters:
- *     - name: userId
- *        in: path
- *        required: true
- *        description: ID of user to return.
- *        schema:
- *           type: integer
- *        example: 1
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          description: ID of user to return.
+ *          schema:
+ *             type: integer
+ *             format: int64
  *     responses:
- *       200:
+ *       '200':
  *         description: successful operation
- *         schema:
- *           $ref: #/definitions/users
- *       400:
+ *         content:
+ *            application/json:
+ *               schema:
+ *                 $ref: #/components/schemas/users
+ *       '400':
  *         description: The specified user ID is invalid (not a number).
- *       404:
+ *       '404':
  *         description: A user with the specified ID was not found.
- *       default:
+ *       '500':
+ *         description: error of server.
+ *       'default':
  *         description: Unexpected error
- *     security:
- *     - api_key: []
  */
 router.get("/:id", validateRequestSchema(getByIdSchema), getUserById);
 router.put("/:id", validateRequestSchema(editUserSchema), editUser);
