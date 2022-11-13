ee2f939276637eb2553f3ab41d439cb44a912b4c
Agustin Canteros <canterosagustindev@gmail.com>
2022-11-13 16:32:45 -0300

    fix schema editUser

--- schemas/user/editUserSchema.js
+++ schemas/user/editUserSchema.js
@@ -11,4 +11,28 @@ module.exports = {
       }
     }
   },
+  firstName: {
+    notEmpty: {
+      errorMessage:"firstName is null"
+    }
+  },
+  lastName: {
+    notEmpty:{
+      errorMessage:"lastName is null"
+    }
+  },
+  password: {
+    notEmpty:{
+      errorMessage:"password is null"
+    }
+  },
+  email: {
+    notEmpty:{
+      errorMessage:"email is null"
+    },   
+    isEmail: {
+      errorMessage: "please enter a correct email"
+    },
+  }
+
 }
